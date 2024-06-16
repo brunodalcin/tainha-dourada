de2bf0259722865b039185256a9cc6ffefaa6641
Dalcin <brunodalcin5@gmail.com>
2024-06-16 16:27:46 -0300

    adjust adm & common access on rating

--- components/rating/rating-details/rating-details.js
+++ components/rating/rating-details/rating-details.js
@@ -25,6 +25,7 @@ customElements.define(
         return;
       }
 
+      const isAdm = user.login === 'adm'; // Verifica se o usuário é admin
       const movieId = movie;
       const matriculaFiles = await this.getMatriculaFiles(this._movieData.titulo);
 
@@ -34,8 +35,9 @@ customElements.define(
         <link rel="stylesheet" href="/components/rating/rating-details/rating-details.css">
         <div class="container align-items-center">
           <div>
-              <button type="button" id="voltar">Voltar a listagem</button>
+            <button type="button" id="voltar">Voltar a listagem</button>
           </div>
+          ${isAdm ? `
           <div class="form-check d-flex mt-1">     
             <input class="form-check-input-lg" type="checkbox" id="participante" ${
               this._movieData.PARTICIPANTE ? "checked" : ""
@@ -43,97 +45,129 @@ customElements.define(
             <label class="form-check-label-lg ms-1" for="participante">
               Apto a participar?
             </label>           
-          </div>
+          </div>` : ""}
           <div class="form-check d-flex mt-1">     
             <input class="form-check-input-lg" type="checkbox" id="pepa" ${
               this._movieData.PEPA ? "checked" : ""
-            }>
+            } ${!isAdm ? "disabled" : ""}>
             <label class="form-check-label-lg ms-1" for="pepa">
               É um Pepa?
             </label>           
           </div>
         </div>
-        <div class="container">
-          <img class="cartaz" src="${this._movieData.cartaz}" alt="${this._movieData.titulo}">
-          <div class="row">
-            <div class="col-md-6">
-              <h4 class="text-primary">Deixe sua Nota</h4>
-              <div class="form-group">
-                <label for="n1">Aspectos Técnicos</label>
-                <select id="n1" class="form-control" ${userRating ? "disabled" : ""}>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                </select>
-                <label for="n2">Concepção Estética</label>              
-                <select id="n2" class="form-control" ${userRating ? "disabled" : ""}>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </select>
-                <label for="n3">Relevância do Tema</label>              
-                <select id="n3" class="form-control" ${userRating ? "disabled" : ""}>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </select>                            
-              </div>
-              <button type="button" id="submit-rating" class="btn btn-primary" ${userRating ? "disabled" : ""}>Enviar Nota</button>
-              <h4 class="text-primary">Informações do Curta-metragem</h4>
-              ${this.createInput("Título", "titulo", this._movieData.titulo)}            
-              ${this.createInput("Link de Acesso", "link-acesso", this._movieData["link-acesso"])}
-              ${this.createInput("Observações de Acesso", "obs-acesso", this._movieData["obs-acesso"])}
-              ${this.createInput("Gênero", "genero", this._movieData.genero)}
-              <h4 class="text-primary">Dados do Cadastrante</h4>
-              ${this.createInput("Nome", "nome", this._movieData.nome)}
-              ${this.createInput("Endereço", "endereco", this._movieData.endereco)}
-              ${this.createInput("Cidade", "cidade", this._movieData.cidade)}
-              ${this.createInput("Estado", "estado", this._movieData.estado)}
-              ${this.createInput("País", "pais", this._movieData.pais)}
-              ${this.createInput("Email", "email", this._movieData.email)}
-              ${this.createInput("CEP", "cep", this._movieData.cep)}
-              ${this.createInput("Telefone", "telefone", this._movieData.telefone)}            
-            </div>        
-            <div class="col-md-6">
-              <h4 class="text-primary">Documentos</h4>
-              ${this.createInput("Cartaz", "cartaz", this._movieData.cartaz)}            
-              ${this.createInput("Ficha de Autorização", "autorizacao", this._movieData.autorizacao)}
-              ${this.createInput("Matrícula", "matricula", matriculaFiles)}    
-              <h4 class="text-primary">Ficha Técnica<br></h4>
-              ${this.createInput("Produtor", "produtor", this._movieData.produtor)}
-              ${this.createInput("Direção", "direcao", this._movieData.direcao)}
-              ${this.createInput("Roteirista", "roteirista", this._movieData.roteirista)}
-              ${this.createInput("Trilha", "trilha", this._movieData.trilha)}
-              ${this.createInput("Fotografia", "fotografia", this._movieData.fotografia)}
-              ${this.createInput("Arte", "arte", this._movieData.arte)}            
-              ${this.createInput("Editor de Som", "editor_som", this._movieData.editor_som)}
-              ${this.createInput("Editor de Vídeo", "editor_video", this._movieData.editor_video)}
-              ${this.createInput("Elenco", "elenco", this._movieData.elenco)}                    
-            </div>
-          </div>      
-        </div>    
+
       `;
 
-      if (userRating) {
-        this.querySelector("#n1").value = userRating.aspectoTecnico;
-        this.querySelector("#n2").value = userRating.concepcaoEstetica;
-        this.querySelector("#n3").value = userRating.relevanciaTema;
+      // Conteúdo específico para administradores
+      if (isAdm) {
+        this.innerHTML += `
+        <div class="container">
+          <img class="cartaz" src="${this._movieData.cartaz}" alt="${this._movieData.titulo}">        
+            <div class="row">
+              <div class="col-md-6">
+                <h4 class="text-primary">Informações do Curta-metragem</h4>
+                ${this.createInput("Título", "titulo", this._movieData.titulo)}            
+                ${this.createInput("Link de Acesso", "link-acesso", this._movieData["link-acesso"])}
+                ${this.createInput("Observações de Acesso", "obs-acesso", this._movieData["obs-acesso"])}
+                ${this.createInput("Gênero", "genero", this._movieData.genero)}
+                <h4 class="text-primary">Dados do Cadastrante</h4>
+                ${this.createInput("Nome", "nome", this._movieData.nome)}
+                ${this.createInput("Endereço", "endereco", this._movieData.endereco)}
+                ${this.createInput("Cidade", "cidade", this._movieData.cidade)}
+                ${this.createInput("Estado", "estado", this._movieData.estado)}
+                ${this.createInput("País", "pais", this._movieData.pais)}
+                ${this.createInput("Email", "email", this._movieData.email)}
+                ${this.createInput("CEP", "cep", this._movieData.cep)}
+                ${this.createInput("Telefone", "telefone", this._movieData.telefone)}            
+              </div>
+              <div class="col-md-6">
+                <h4 class="text-primary">Documentos</h4>
+                ${this.createInput("Cartaz", "cartaz", this._movieData.cartaz)}            
+                ${this.createInput("Ficha de Autorização", "autorizacao", this._movieData.autorizacao)}
+                ${this.createInput("Matrícula", "matricula", matriculaFiles)}    
+                <h4 class="text-primary">Ficha Técnica<br></h4>
+                ${this.createInput("Produtor", "produtor", this._movieData.produtor)}
+                ${this.createInput("Direção", "direcao", this._movieData.direcao)}
+                ${this.createInput("Roteirista", "roteirista", this._movieData.roteirista)}
+                ${this.createInput("Trilha", "trilha", this._movieData.trilha)}
+                ${this.createInput("Fotografia", "fotografia", this._movieData.fotografia)}
+                ${this.createInput("Arte", "arte", this._movieData.arte)}            
+                ${this.createInput("Editor de Som", "editor_som", this._movieData.editor_som)}
+                ${this.createInput("Editor de Vídeo", "editor_video", this._movieData.editor_video)}
+                ${this.createInput("Elenco", "elenco", this._movieData.elenco)}                    
+              </div>
+            </div>      
+          </div>
+        `;
       } else {
+        // Conteúdo específico para usuários comuns
+        this.innerHTML += `
+        <div class="container">
+          <img class="cartaz" src="${this._movieData.cartaz}" alt="${this._movieData.titulo}">        
+            <div class="row" style="width:100%">
+              <div class="col-md-6">            
+                <h4 class="text-primary">Informações do Curta-metragem</h4>
+                ${this.createInput("Título", "titulo", this._movieData.titulo)}            
+                ${this.createInput("Link de Acesso", "link-acesso", this._movieData["link-acesso"])}
+                ${this.createInput("Observações de Acesso", "obs-acesso", this._movieData["obs-acesso"])}
+                ${this.createInput("Gênero", "genero", this._movieData.genero)}              
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="n1">Aspectos Técnicos </label>
+                  <div class="star-rating">
+                    ${this.createStars("n1", userRating ? userRating.aspectoTecnico : 0, 4)}
+                  </div>
+                  <label for="n2">Concepção Estética</label>
+                  <div class="star-rating">
+                    ${this.createStars("n2", userRating ? userRating.concepcaoEstetica : 0, 3)}
+                  </div>
+                  <label for="n3">Relevância do Tema</label>
+                  <div class="star-rating">
+                    ${this.createStars("n3", userRating ? userRating.relevanciaTema : 0, 3)}
+                  </div>
+                </div>
+                <button type="button" id="submit-rating" class="btn btn-primary" ${userRating ? "disabled" : ""}>Enviar Notas</button>
+              </div>
+            </div>      
+          </div>
+        `;
+      }
+
+      if (userRating) {
+        this.querySelectorAll(`input[name="n1"][value="${userRating.aspectoTecnico}"]`).forEach(el => el.checked = true);
+        this.querySelectorAll(`input[name="n2"][value="${userRating.concepcaoEstetica}"]`).forEach(el => el.checked = true);
+        this.querySelectorAll(`input[name="n3"][value="${userRating.relevanciaTema}"]`).forEach(el => el.checked = true);
+      } else if (!isAdm) {
         this.querySelector('#submit-rating').addEventListener('click', () => this.submitRating(user.uid, movieId));
       }
 
-      // Checkboxes
-      document.querySelector("#participante").addEventListener("change", () => {
-        this.updateCheckboxField(movieId, "participante", "PARTICIPANTE");
-      });
-      document.querySelector("#pepa").addEventListener("change", () => {
-        this.updateCheckboxField(movieId, "pepa", "PEPA");
-      });
+      if (isAdm) {
+        // Checkboxes
+        document.querySelector("#participante").addEventListener("change", () => {
+          this.updateCheckboxField(movieId, "participante", "PARTICIPANTE");
+        });
+        document.querySelector("#pepa").addEventListener("change", () => {
+          this.updateCheckboxField(movieId, "pepa", "PEPA");
+        });
+      }
 
       // Voltar a listagem de filmes
       this.querySelector("#voltar").addEventListener("click", () => loadComponent('rating'));
     }
+    createStars(name, rating, nrStars) {
+      let stars = '';
+      for (let i = nrStars; i > 0; i--) {
+        const id = `${name}_${i}`; // Criar ID único
+        stars += `
+          <input type="radio" id="${id}" name="${name}" value="${i}" ${rating === i ? 'checked' : ''} />
+          <label for="${id}">&#9733;</label>
+        `;
+      }
+      return stars;
+    }
+    
+    
 
     createInput(label, id, values) {
       if (id === "matricula" && !Array.isArray(values)) {
@@ -180,82 +214,78 @@ customElements.define(
       if (!movieId) {
         return null;
       }
-
       return movieId;
     }
 
-    async submitRating(userId, movieId) {
-      const n1 = this.querySelector("#n1").value;
-      const n2 = this.querySelector("#n2").value;
-      const n3 = this.querySelector("#n3").value;
-
-      const rating = {
-        userId,
-        movieId,
-        aspectoTecnico: parseInt(n1),
-        concepcaoEstetica: parseInt(n2),
-        relevanciaTema: parseInt(n3),
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      };
-
-      try {
-        await firebase.firestore().collection("RATINGS").add(rating);
-        alert("Nota enviada com sucesso!");
-        this.render(); // Re-render to update UI and disable fields
+    async getMatriculaFiles(titulo) {
+      const storageRef = firebase.storage().ref(`/uploads/${titulo}`);
+        try {
+          const files = await storageRef.listAll();
+          const matriculaFiles = files.items.filter(item => item.name.startsWith('matricula'));
+          const downloadUrls = await Promise.all(matriculaFiles.map(file => file.getDownloadURL()));
+          return downloadUrls;
       } catch (error) {
-        console.error("Erro ao enviar a nota:", error);
+          console.error('Erro ao obter arquivos de matrícula:', error);
       }
+
+      return [];
     }
+
+
 
     async getUserRating(userId, movieId) {
       try {
-        const ratingsSnapshot = await firebase
-          .firestore()
+        const ratingDoc = await firebase.firestore()
           .collection("RATINGS")
           .where("userId", "==", userId)
           .where("movieId", "==", movieId)
           .get();
 
-        if (ratingsSnapshot.empty) {
-          return null;
+        if (!ratingDoc.empty) {
+          return ratingDoc.docs[0].data();
         }
-
-        const rating = ratingsSnapshot.docs[0].data();
-        return {
-          aspectoTecnico: rating.aspectoTecnico,
-          concepcaoEstetica: rating.concepcaoEstetica,
-          relevanciaTema: rating.relevanciaTema,
-        };
       } catch (error) {
-        console.error("Erro ao obter a nota do usuário:", error);
-        return null;
+        console.error("Erro ao obter a avaliação do usuário:", error);
+      }
+      return null;
+    }
+
+    async submitRating(userId, movieId) {
+      const aspectoTecnico = parseInt(this.querySelector(`input[name="n1"]:checked`).value);
+      const concepcaoEstetica = parseInt(this.querySelector(`input[name="n2"]:checked`).value);
+      const relevanciaTema = parseInt(this.querySelector(`input[name="n3"]:checked`).value);
+
+      const ratingData = {
+        userId: userId,
+        movieId: movieId,
+        aspectoTecnico: aspectoTecnico,
+        concepcaoEstetica: concepcaoEstetica,
+        relevanciaTema: relevanciaTema,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      };
+
+      try {
+        await firebase.firestore().collection("RATINGS").add(ratingData);
+        alert("Avaliação enviada com sucesso!");
+        this.render();
+      } catch (error) {
+        console.error("Erro ao enviar a avaliação:", error);
+        alert("Erro ao enviar a avaliação. Tente novamente.");
       }
     }
 
-    async getMatriculaFiles(titulo) {
-      const storageRef = firebase.storage().ref();
-      const listRef = storageRef.child(titulo);
+    async updateCheckboxField(movieId, checkboxId, firestoreField) {
+      const isChecked = document.querySelector(`#${checkboxId}`).checked;
       try {
-        const res = await listRef.listAll();
-        const urls = await Promise.all(res.items.map((itemRef) => itemRef.getDownloadURL()));
-        return urls;
-      } catch (error) {
-        console.error("Erro ao listar os arquivos de matrícula:", error);
-        return [];
-      }
-    }
+        await firebase.firestore()
+          .collection("FORMS")
+          .doc(movieId)
+          .update({ [firestoreField]: isChecked });
 
-    async updateCheckboxField(movieId, checkboxId, fieldName) {
-      const checkbox = document.querySelector(`#${checkboxId}`);
-      const isChecked = checkbox.checked;
-      const firestore = firebase.firestore();
-      const movieRef = firestore.collection("FORMS").doc(movieId);
-    
-      try {
-        await movieRef.update({ [fieldName]: isChecked });
-        console.log(`Campo ${fieldName} atualizado com sucesso!`);
+        alert(`Campo ${firestoreField} atualizado com sucesso!`);
+        console.log(`Campo ${firestoreField} atualizado com sucesso.`);
       } catch (error) {
-        console.error(`Erro ao atualizar o campo ${fieldName}:`, error);
+        console.error(`Erro ao atualizar o campo ${firestoreField}:`, error);
       }
     }
   }
