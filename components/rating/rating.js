3f5b8f99c9ae1d526d332b18ac77f60b8acd7cd2
Dalcin <brunodalcin5@gmail.com>
2024-06-17 22:00:14 -0300

    add movies img lazy loading

--- components/rating/rating.js
+++ components/rating/rating.js
@@ -1,136 +1,172 @@
-customElements.define('rating-component', class extends HTMLElement {
-  async connectedCallback() {
-    const db = firebase.firestore();
-    const formsRef = db.collection('FORMS');
+customElements.define(
+  "rating-component",
+  class extends HTMLElement {
+    async connectedCallback() {
+      const db = firebase.firestore();
+      const formsRef = db.collection("FORMS");
+      const user = await getCurrentUser();
+      const isAdm = user.login === "adm";
 
-    // Verificando se é ADM
+      let selectedGenre = null;
 
-    const user = await getCurrentUser();
-    const isAdm = user.login === 'adm';
+      this.innerHTML = `
+  <link rel="stylesheet" href="/components/rating/rating.css">
+  <header>
+    <img src="assets/TAINHA-BRANCO-TRANSPARENTE.png" alt="Logo">           
+        <div btn-group d-flex " ${isAdm ? "" : 'style="display:none"'} ">
+          <button id="ranking-btn" class="btn btn-primary">Ver Rankings</button>                
+        </div>       
+  </header>                
+  <div class="btn-group d-flex" role="group" aria-label="Basic radio toggle button group">
+    <input type="radio" class="btn-check" name="genre" id="btn-check-todos" autocomplete="off" value="todos" checked>
+    <label class="btn btn-outline-primary" for="btn-check-todos">Todos</label>
 
-    let selectedGenre = null;
+    <input type="radio" class="btn-check" name="genre" id="btn-check-clipe" autocomplete="off" value="Videoclipe">
+    <label class="btn btn-outline-primary" for="btn-check-clipe">Videoclipe</label>
 
-    this.innerHTML = `
-      <link rel="stylesheet" href="/components/rating/rating.css">
-      <header>
-        <img src="assets/TAINHA-BRANCO-TRANSPARENTE.png" alt="Logo">           
-            <div btn-group d-flex " ${isAdm ? '' : 'style="display:none"'} ">
-              <button id="ranking-btn" class="btn btn-primary">Ver Rankings</button>                
-            </div>       
-      </header>                
-      <div class="btn-group d-flex" role="group" aria-label="Basic radio toggle button group">
-        <input type="radio" class="btn-check" name="genre" id="btn-check-todos" autocomplete="off" value="todos" checked>
-        <label class="btn btn-outline-primary" for="btn-check-todos">Todos</label>
+    <input type="radio" class="btn-check" name="genre" id="btn-check-doc" autocomplete="off" value="Documentário">
+    <label class="btn btn-outline-primary" for="btn-check-doc">Documentário</label>
 
-        <input type="radio" class="btn-check" name="genre" id="btn-check-clipe" autocomplete="off" value="Videoclipe">
-        <label class="btn btn-outline-primary" for="btn-check-clipe">Videoclipe</label>
+    <input type="radio" class="btn-check" name="genre" id="btn-check-fic" autocomplete="off" value="Ficção">
+    <label class="btn btn-outline-primary" for="btn-check-fic">Ficção</label>
 
-        <input type="radio" class="btn-check" name="genre" id="btn-check-doc" autocomplete="off" value="Documentário">
-        <label class="btn btn-outline-primary" for="btn-check-doc">Documentário</label>
+    <input type="radio" class="btn-check" name="genre" id="btn-check-pep" autocomplete="off" value="Pepa">
+    <label class="btn btn-outline-primary" for="btn-check-pep">Pepa</label>
+  </div>      
+    <h1>CURTAS CONCORRENTES</h1>
 
-        <input type="radio" class="btn-check" name="genre" id="btn-check-fic" autocomplete="off" value="Ficção">
-        <label class="btn btn-outline-primary" for="btn-check-fic">Ficção</label>
+  <div class="container">
+    <div class="row" id="movie-list"></div>
+  </div>
+`;
 
-        <input type="radio" class="btn-check" name="genre" id="btn-check-pep" autocomplete="off" value="Pepa">
-        <label class="btn btn-outline-primary" for="btn-check-pep">Pepa</label>
-      </div>      
-        <h1>CURTAS CONCORRENTES</h1>
-
-      <div class="container">
-        <div class="row" id="movie-list"></div>
-      </div>
-    `; 
-
-
-    const renderMovies = async () => {
-      let query = formsRef;
-      if (selectedGenre && selectedGenre !== 'todos') {
-        if (selectedGenre === 'Pepa') {
-          query = query.where('PEPA', '==', true);
-        } else {
-          query = query.where('genero', '==', selectedGenre);
-        }
-      }
-
-      try {
-        const querySnapshot = await query.get();
-        const moviesMap = {};
-        querySnapshot.forEach((doc) => {
-          const movie = doc.data();
-          // impede cadastros duplicados
-          if (!moviesMap[movie.titulo]) {
-            moviesMap[movie.titulo] = movie;
+      const renderMovies = async () => {
+        let query = formsRef;
+        if (selectedGenre && selectedGenre !== "todos") {
+          if (selectedGenre === "Pepa") {
+            query = query.where("PEPA", "==", true);
+          } else {
+            query = query.where("genero", "==", selectedGenre);
           }
-        });
-        const movies = Object.values(moviesMap);
-        const movieList = document.getElementById('movie-list');
-        movieList.innerHTML = '';
-        movies.forEach(movie => {
-          const col = document.createElement('div');
-          col.classList.add('col-md-3');
-          col.innerHTML = `
-            <div class="card" data-titulo="${movie.titulo}">
-              <img src="${movie.cartaz}" class="card-img-top movie-img" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${movie.titulo}</h5>
-                <p class="card-text">Direção: ${movie.direcao}</p>
-                <p class="card-text text-end">Gênero: ${movie.genero}</p>                      
-              </div>
-            </div>
-          `;
-          movieList.appendChild(col);
-        });
+        }
 
-        // Adiciona os event listeners após os filmes serem renderizados
-        this.querySelectorAll('.card').forEach(card => {
-          card.addEventListener('click', async () => {
-            const titulo = card.getAttribute('data-titulo');
-            const selectedMovie = moviesMap[titulo];
-            const contentContainer = document.getElementById('content-container');
-            contentContainer.innerHTML = ''; // Limpa o conteúdo atual
+        try {
+          const querySnapshot = await query.get();
+          const moviesMap = {};
+          querySnapshot.forEach((doc) => {
+            const movie = doc.data();
+            if (!moviesMap[movie.titulo]) {
+              moviesMap[movie.titulo] = movie;
+            }
+          });
+          const movies = Object.values(moviesMap);
+          const movieList = document.getElementById("movie-list");
+          movieList.innerHTML = "";
+          movies.forEach((movie) => {
+            const col = document.createElement("div");
+            col.classList.add("col-md-3");
+            col.innerHTML = `
+        <div class="card" data-titulo="${movie.titulo}">
+          <img data-src="${movie.cartaz}" class="card-img-top movie-img" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${movie.titulo}</h5>
+            <p class="card-text">Direção: ${movie.direcao}</p>
+            <p class="card-text text-end">Gênero: ${movie.genero}</p>                      
+          </div>
+        </div>
+      `;
+            movieList.appendChild(col);
+          });
 
-            try {
-              const movieSnapshot = await firebase.firestore().collection('FORMS').where('titulo', '==', titulo).get();
-              if (movieSnapshot.empty) {
-                throw new Error('Filme não encontrado');
-              }
+          // Lazy loading das imagens
+          const lazyLoadImages = () => {
+            const images = document.querySelectorAll("img[data-src]");
+            const config = {
+              rootMargin: "0px 0px 50px 0px",
+              threshold: 0.01,
+            };
 
-              const movieDoc = movieSnapshot.docs[0];
-              const movieId = movieDoc.id;
-              localStorage.setItem('currentMovieId', movieId); // Armazena o ID do filme escolhido no localStorage
-            } catch (error) {
-              console.error('Erro ao obter o ID do filme:', error);
+            let observer;
+            if ("IntersectionObserver" in window) {
+              observer = new IntersectionObserver(onIntersection, config);
+              images.forEach((image) => {
+                observer.observe(image);
+              });
+            } else {
+              images.forEach((image) => {
+                loadImage(image);
+              });
             }
 
-            const ratingDetails = document.createElement('rating-details-component');
-            ratingDetails.movieData = selectedMovie;
-            contentContainer.appendChild(ratingDetails);
+            function onIntersection(entries, observer) {
+              entries.forEach((entry) => {
+                if (entry.intersectionRatio > 0) {
+                  observer.unobserve(entry.target);
+                  loadImage(entry.target);
+                }
+              });
+            }
 
-            window.scrollTo({ top: 0, behavior: 'smooth' });
+            function loadImage(image) {
+              image.src = image.getAttribute("data-src");
+              image.removeAttribute("data-src");
+            }
+          };
+          lazyLoadImages();
+
+          // Adiciona os event listeners após os filmes serem renderizados
+          this.querySelectorAll(".card").forEach((card) => {
+            card.addEventListener("click", async () => {
+              const titulo = card.getAttribute("data-titulo");
+              const selectedMovie = moviesMap[titulo];
+              const contentContainer =
+                document.getElementById("content-container");
+              contentContainer.innerHTML = "";
+
+              try {
+                const movieSnapshot = await firebase
+                  .firestore()
+                  .collection("FORMS")
+                  .where("titulo", "==", titulo)
+                  .get();
+                if (movieSnapshot.empty) {
+                  throw new Error("Filme não encontrado");
+                }
+
+                const movieDoc = movieSnapshot.docs[0];
+                const movieId = movieDoc.id;
+                localStorage.setItem("currentMovieId", movieId);
+              } catch (error) {
+                console.error("Erro ao obter o ID do filme:", error);
+              }
+
+              const ratingDetails = document.createElement(
+                "rating-details-component"
+              );
+              ratingDetails.movieData = selectedMovie;
+              contentContainer.appendChild(ratingDetails);
+
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            });
           });
-        });
+        } catch (error) {
+          console.error("Erro ao carregar filmes:", error);
+          this.innerHTML = "<p>Ocorreu um erro ao carregar os filmes.</p>";
+        }
+      };
 
-      } catch (error) {
-        console.error('Erro ao carregar filmes:', error);
-        this.innerHTML = '<p>Ocorreu um erro ao carregar os filmes.</p>';
-      }
-    };
+      renderMovies();
 
-    renderMovies();
+      this.querySelector("#ranking-btn").addEventListener("click", () => {
+        loadComponent("ranking");
+      });
 
-
-    // Adiciona o evento de clique ao botão de ranking
-    this.querySelector('#ranking-btn').addEventListener('click', () => {
-      loadComponent('ranking');
-    });
-  
-
-    document.addEventListener('change', (event) => {
-      if (event.target && event.target.name === 'genre') {
-        selectedGenre = event.target.value;
-        renderMovies();
-      }
-    });
+      document.addEventListener("change", (event) => {
+        if (event.target && event.target.name === "genre") {
+          selectedGenre = event.target.value;
+          renderMovies();
+        }
+      });
+    }
   }
-});
-
+);
